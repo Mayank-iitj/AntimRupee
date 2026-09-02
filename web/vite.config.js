f7f511fc847786e03abb8783706e2562e09ce61f
User <user@example.com>
2026-09-02 15:27:10 +0530

    fix: increase PWA maximumFileSizeToCacheInBytes limit to fix vercel build

--- web/vite.config.js
+++ web/vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
